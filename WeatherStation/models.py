d07da633a74508fe639d32f7d49778395ebdb0d3
EvanHauck <evan.hauck5@gmail.com>
2017-03-12 11:37:49 -0700

    added db model code

--- WeatherStation/models.py
+++ WeatherStation/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Record(models.Model):
+    timeStamp = models.DateTimeField()
+    recordNum = models.IntegerField(primary_key=True)
+    battAvg = models.DecimalField(max_digits=4,decimal_places=2)
+    pTempCAvg = models.DecimalField(max_digits=4,decimal_places=2)
+    airTCAvg = models.DecimalField(max_digits=4,decimal_places=2)
+    rH = models.DecimalField(max_digits=4,decimal_places=2)
+    slrkW = models.DecimalField(max_digits=4,decimal_places=2)
+    slrMJTot = models.DecimalField(max_digits=8,decimal_places=6)
+    wSMs = models.DecimalField(max_digits=6,decimal_places=3)
+    windDir = models.DecimalField(max_digits=5,decimal_places=1)
+    pARDen = models.IntegerField()
+    pARTotTot = models.DecimalField(max_digits=10,decimal_places=2)
+    bPMmHg = models.DecimalField(max_digits=6,decimal_places=1)
+    rainMmTot = models.IntegerField()
+
+    class Meta:
+        ordering = ['-recordNum']
+
+    def __str__(self):
+        return self.recordNum
