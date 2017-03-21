963a56e72aa166babe6c741bda59535bb43be3ae
zoothar <cory.gomez541@myci.csuci.edu>
2017-03-20 18:22:47 -0700

    deleted ESRM_SIERRA

--- WeatherStation/static/js/Main_Script.js
+++ WeatherStation/static/js/Main_Script.js
@@ -1,18 +1,7 @@
 /**
  * Created by batman on 3/15/17.
  */
-var today = new Date();
-var dd  = today.getDate();
-var mm = today.getMonth()+1;
-var yyyy = today.getFullYear();
-if(dd<10){
-    dd='0'+ dd;
-}
-if(mm<10){
-    mm='0'+ mm;
-};
-today = yyyy + '-' + mm + '-' + dd;
-document.getElementById("dateField").setAttribute("max", today);
+
 
 function update(){
     alert_string = '';
