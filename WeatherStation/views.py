4b6cac2e39c0cbc9d53702aa4bd572dd1ac7d4b0
EvanHauck <evan.hauck5@gmail.com>
2017-03-24 14:46:58 -0700

    added connection to django models db, still working on finding where it is stored for sql

--- WeatherStation/views.py
+++ WeatherStation/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib import messages
-
+from .models import Record
 
 
 def index(request):
+    #models db connector
+    all_records = Record.objects.all()
     t = loader.get_template('Main_Page.html')
     c = {'foo': 'bar'}
     messages.add_message(request, messages.INFO, 'Hello world.')
