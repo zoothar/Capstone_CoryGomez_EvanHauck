8f00ee17e2d52c45aa59dff94a58c64750f531fb
zoothar <cory.gomez541@myci.csuci.edu>
2017-03-28 23:25:11 -0700

    Cleaned up the views

--- WeatherStation/views.py
+++ WeatherStation/views.py
@@ -1,16 +1,13 @@
 from django.http import HttpResponse
-from django.template import loader
-from django.contrib import messages
+from django.shortcuts import render
 from .models import Record
 
 
 def index(request):
     #models db connector
     all_records = Record.objects.all()
-    t = loader.get_template('Main_Page.html')
-    c = {'foo': 'bar'}
-    messages.add_message(request, messages.INFO, 'Hello world.')
-    return HttpResponse(t.render(c, request))
+    context = {'all_record': all_records}
+    return render(request, 'Main_Page.html', context)
 
 
 def plot(request, plot_id):
