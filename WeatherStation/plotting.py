790fa3177624fab43688c3ada35c1f9c34a5b2ab
EvanHauck <evan.hauck5@gmail.com>
2017-03-29 09:26:26 -0700

    changed html string to file export instead of console print

--- WeatherStation/plotting.py
+++ WeatherStation/plotting.py
@@ -9,12 +9,12 @@ import sqlite3
 #commented out evan's personal settings and replaced with mine, evan if you want yours to work you have to uncomment your code and comment out mine...
 
 #Evan's
-#conn = sqlite3.connect('/home/evan/Documents/Capstone_randomFiles/CapstoneProject/Capstone_CoryGomez_EvanHauck/db.ESRM_Sierra')
+conn = sqlite3.connect('/home/evan/Documents/Capstone_randomFiles/CapstoneProject/Capstone_CoryGomez_EvanHauck/db.ESRM_Sierra')
 
 #Cory's
-conn = sqlite3.connect('/home/batman/Documents/CSUCI/Capstone/Capstone_CoryGomez_EvanHauck/db.ESRM_Sierra')
+#conn = sqlite3.connect('/home/batman/Documents/CSUCI/Capstone/Capstone_CoryGomez_EvanHauck/db.ESRM_Sierra')
 
-df = pd.read_sql_query('SELECT timeStamp, battAvg FROM WeatherStation_record', conn)
+df = pd.read_sql_query('SELECT timeStamp, battAvg FROM WeatherStation_record WHERE recordNum < 15', conn)
 #print(df.head()) #for testing connection
 
 file_str = offline.plot({
@@ -28,4 +28,7 @@ file_str = offline.plot({
                 'layout': go.Layout(xaxis=go.XAxis(title='Date'), yaxis=go.YAxis(title='Voltage', type='log'))
             }, output_type='div')
 #file_loc = offline.plot({'data': [{'y': [4, 2, 3, 4]}],'layout': {'title': 'Test Plot','font': dict(size=16)}},image='png') #test example
-print(file_str)
+f = open("/home/evan/Documents/test_string_file.txt","w")
+
+print(f)
+f.write(file_str)
