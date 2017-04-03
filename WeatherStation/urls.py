25d9e96b2e21c8ace7fdc7c741757a36be6d035a
zoothar <cory.gomez541@myci.csuci.edu>
2017-04-03 13:15:47 -0700

    It Works!!!!

--- WeatherStation/urls.py
+++ WeatherStation/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^plot/', views.plot, name='plot'),
-    url(r'^submit', views.submit, name='submit')
+    url(r'^plot', views.plot, name='plot'),
+    #url(r'^submit', views.submit, name='submit')
 ]
--- WeatherStation/views.py
+++ WeatherStation/views.py
@@ -3,34 +3,32 @@ from django.shortcuts import render
 from . plotting import plotGraph
 from datetime import datetime
 
+
 def index(request):
     now = datetime.now()
     end = datetime.__str__(now)
+    plotName = 'plot'
     #models db connector
     context = {
-        'enddate': end,
+        'endDate': end,
+        'plots': plotName
                }
     return render(request, 'Main_Page.html', context)
 
 
 def plot(request):
-    column = 0
-    start = "2016-03-01"
-    now = datetime.now()
-    end = datetime.__str__(now)
+    column = request.POST.get('column')
+    start = request.POST.get('dateField1')
+    end = request.POST.get('dateField2')
+    if column== None:
+        column = '0'
+    if start== None:
+        start = "2016-03-01"
+    if end== None:
+        now = datetime.now()
+        end = datetime.__str__(now)
     context = {
         'plotting': plotGraph(column, start, end)
     }
     return render(request, 'Plot_Page.html', context)
 
-def submit(request):
-    column = request.POST.get('column')
-    start = request.POST.get('dateField1')
-    end = request.POST.get('dateField2')
-    #str = column + start + end
-    #print(str)
-    context = {
-        'plotting': plotGraph(column, start, end)
-    }
-    #str = '<html><body>' + plotGraph(plotGraph(column, start, end)) + '</body></html'
-    return HttpResponseRedirect('/main/submit/',context)
