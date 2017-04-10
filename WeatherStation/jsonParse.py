698548539bef21bb27bf3b6925144ff674955493
EvanHauck <evan.hauck5@gmail.com>
2017-04-10 11:25:03 -0700

    turned jsonParse code into a function to be called by timer

--- WeatherStation/jsonParse.py
+++ WeatherStation/jsonParse.py
@@ -5,38 +5,38 @@ import django
 django.setup()
 from WeatherStation.models import Record
 
-last_rec = Record.objects.latest('recordNum')
-print(last_rec.recordNum)
-com = "http://10.121.1.194/?command=dataquery&uri=dl:Table1&table=Table1&format=json&mode=since-record&p1=" + str(last_rec.recordNum)
-print(com)
-with urllib.request.urlopen(com) as url:
-    j = json.loads(url.read().decode())
+def queryWeatherStation():
+    last_rec = Record.objects.latest('recordNum')
+    print(last_rec.recordNum)
+    com = "http://10.121.1.194/?command=dataquery&uri=dl:Table1&table=Table1&format=json&mode=since-record&p1=" + str(last_rec.recordNum)
+    print(com)
+    with urllib.request.urlopen(com) as url:
+        j = json.loads(url.read().decode())
 
-# http://10.121.1.194/?command=dataquery&uri=dl:Table1&table=Table1&format=json&mode=since-record&p1=X   -where X is last record recorded
+    # http://10.121.1.194/?command=dataquery&uri=dl:Table1&table=Table1&format=json&mode=since-record&p1=X   -where X is last record recorded
 
-record = Record()
+    record = Record()
 
-# extracting data (formatting the time properly) and importing into Record models db
-num_recs = len(j["data"])
-print(str(num_recs))
-count = 0
-while count < num_recs:
-    dt = j['data'][count]['time']
-    record.timeStamp = dt.replace('T', ' ')
-    record.recordNum = j['data'][count]['no']
-    record.battAvg = j['data'][count]['vals'][0]
-    record.pTempCAvg = j['data'][count]['vals'][1]
-    record.airTCAvg = j['data'][count]['vals'][2]
-    record.rH = j['data'][count]['vals'][3]
-    record.slrkW = j['data'][count]['vals'][4]
-    record.slrMJTot = j['data'][count]['vals'][5]
-    record.wSMs = j['data'][count]['vals'][6]
-    record.windDir = j['data'][count]['vals'][7]
-    record.pARDen = j['data'][count]['vals'][8]
-    record.pARTotTot = j['data'][count]['vals'][9]
-    record.bPMmHg = j['data'][count]['vals'][10]
-    record.rainMmTot = j['data'][count]['vals'][11]
-    print(" recNum = " + str(record.recordNum) + "    timeStamp--->  " +  str(record.timeStamp) )
-    count = count + 1
-    record.save()
-#print(record.bPMmHg)
+    # extracting data (formatting the time properly) and importing into Record models db
+    num_recs = len(j["data"])
+    print(str(num_recs))
+    count = 0
+    while count < num_recs:
+        dt = j['data'][count]['time']
+        record.timeStamp = dt.replace('T', ' ')
+        record.recordNum = j['data'][count]['no']
+        record.battAvg = j['data'][count]['vals'][0]
+        record.pTempCAvg = j['data'][count]['vals'][1]
+        record.airTCAvg = j['data'][count]['vals'][2]
+        record.rH = j['data'][count]['vals'][3]
+        record.slrkW = j['data'][count]['vals'][4]
+        record.slrMJTot = j['data'][count]['vals'][5]
+        record.wSMs = j['data'][count]['vals'][6]
+        record.windDir = j['data'][count]['vals'][7]
+        record.pARDen = j['data'][count]['vals'][8]
+        record.pARTotTot = j['data'][count]['vals'][9]
+        record.bPMmHg = j['data'][count]['vals'][10]
+        record.rainMmTot = j['data'][count]['vals'][11]
+        print(" recNum = " + str(record.recordNum) + "    timeStamp--->  " +  str(record.timeStamp) )
+        count = count + 1
+        record.save()
