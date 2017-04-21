6cd9dc0b85d2947d2c2920a3f10933d8406f5cba
zoothar <cory.gomez541@myci.csuci.edu>
2017-04-21 13:05:03 -0700

    Laptop

--- WeatherStation/tcp_server.py
+++ WeatherStation/tcp_server.py
@@ -3,28 +3,31 @@ from datetime import  datetime
 import django
 django.setup()
 import socket
-from socket import *
+#from socket import *
 from WeatherStation.jsonParse import queryWeatherStation
 
 def runserv():
     # create a socket object
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    #socket()
+    #s = socket(AF_INET, SOCK_STREAM)
+    #s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 
     host = socket.gethostbyname(socket.gethostname())
     print(host)
     port = 12915
 
     # bind to the port
-    s.bind((host, port))
-
-
-    s.listen(1)
-
-    while True:
-        # establish a connection
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen(1)
         clientsocket, addr = s.accept()
-        rsp = 'File received at server at --->  ' + datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        queryWeatherStation(clientsocket.recv(1024000))
-        clientsocket.send(rsp.encode('ascii'))
-        clientsocket.close()
+
+    with clientsocket:
+        while True:
+            # establish a connection
+            rsp = 'File received at server at --->  ' + datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            queryWeatherStation(clientsocket.recv(1024000))
+            clientsocket.send(rsp.encode('ascii'))
+            clientsocket.close()
+
+runserv()
